8d9d0369043cd08e5f68e9e219663ce9a20f0be2
Ilya Vorobyev <ilya.vorobev.2002@list.ru>
2022-11-23 18:23:05 +0500

    Добавлен html-хелпер

---
+++ MiniSurveys.Web/Helpers/HeaderHtmlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MiniSurveys.Web.Helpers
+{
+    public static class HeaderHtmlHelpers
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            var returnActive = (controller == routeController && (action == routeAction || routeAction == "Details"));
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
